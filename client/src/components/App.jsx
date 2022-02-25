44c72713d323c05ebf3abe288fd5aeea1dfb9a63
ChrisRolling <chrisrolling27@gmail.com>
2022-02-25 14:08:12 -0800

    no init data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,16 +11,45 @@ import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 
 const Container = styled.div`
 display: flex;
-
 `;
-
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = initialData;
+    this.state = {
+
+      sessions: {
+        'session-1': { id: 'session-1', content: 'Hello switcher' },
+        'session-2': { id: 'session-2', content: 'Swiper no swiping' },
+        'session-3': { id: 'session-3', content: 'Jezebel was innocent' },
+        'session-4': { id: 'session-4', content: 'One more time with feeling' },
+      },
+
+      columns: {
+        'column-1': {
+          id: 'column-1',
+          title: 'Ideas',
+          sessionIds: ['session-1', 'session-2', 'session-3'],
+        },
+        'column-2': {
+          id: 'column-2',
+          title: 'Notes',
+          sessionIds: [],
+        },
+        'column-3': {
+          id: 'column-3',
+          title: 'Suggestions',
+          sessionIds: ['session-4'],
+        },
+      },
+
+      addSession: false,
+      columnOrder: ['column-1', 'column-2', 'column-3'],
+
+
+    };
 
     this.makeSession = this.makeSession.bind(this);
     this.onDragEnd = this.onDragEnd.bind(this);
@@ -175,40 +204,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const colRef = collection(db, 'posts');
 
-
-//INITIAL DATA
-
-const initialData = {
-
-  sessions: {
-    'session-1': { id: 'session-1', content: 'Hello switcher' },
-    'session-2': { id: 'session-2', content: 'Swiper no swiping' },
-    'session-3': { id: 'session-3', content: 'Jezebel was innocent' },
-    'session-4': { id: 'session-4', content: 'One more time with feeling' },
-  },
-
-  columns: {
-    'column-1': {
-      id: 'column-1',
-      title: 'Ideas',
-      sessionIds: ['session-1', 'session-2', 'session-3', 'session-4'],
-    },
-    'column-2': {
-      id: 'column-2',
-      title: 'Notes',
-      sessionIds: [],
-    },
-    'column-3': {
-      id: 'column-3',
-      title: 'Suggestions',
-      sessionIds: [],
-    },
-  },
-  columnOrder: ['column-1', 'column-2'],
-
-  addSession: false,
-
-};
 
 //old addsession
 
